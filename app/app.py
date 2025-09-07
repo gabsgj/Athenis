39171bfc9e811f3d67e7e574e52d110e78eb05ec
gabsgj <gabriel22dec@gmail.com>
2025-09-07 23:11:43 +0530

    Updated Files

--- app/app.py
+++ app/app.py
@@ -355,7 +355,8 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", "8080"))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 # ---------------------------
 # Factory
